3fcda0a317da4e874df30ae1eed3de48ad353aeb
Vaibhav <vaibhavtiwary7766@gmail.com>
2024-07-11 12:09:54 +0530

    create user model and signup endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,27 +5,35 @@ import authRoutes from "./routes/authroutes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
+
+
 dotenv.config();
-
 const PORT = process.env.PORT || 8000
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
 
+//Middlewares
+app.use(express.json());
 app.use("/api/auth", authRoutes)
 
-app.get("/api/auth/signup", (req, res) => {
-    console.log("signup route")
-})
 
-app.get("/api/auth/login", (req, res) => {
-    console.log("login route")
-})
+// app.get("/", (req, res) => {
+//     res.send("Hello World!");
+// })
 
-app.get("/api/auth/logout", (req, res) => {
-    console.log("logout route")
-})
+
+
+// app.get("/api/auth/signup", (req, res) => {
+//     res.send("signup route")
+// })
+
+// // app.get("/api/auth/login", (req, res) => {
+// //     console.log("login route")
+// // })
+
+// // app.get("/api/auth/logout", (req, res) => {
+// //     console.log("logout route")
+// // })
 
 app.listen(PORT, () => {
-    connectToMongoDB()
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`);
 });
