73108cfb59a15a97a9df537143bed1a6d8ed8778
Vaibhav <vaibhavtiwary7766@gmail.com>
2024-07-11 23:55:14 +0530

    complete ui design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/Signup'
+import Home from './pages/home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
 
     <div className='p-4 h-screen flex items-center justify-center'>
       {/* <Login /> */}
-      <SignUp />
+      {/* <SignUp /> */}
+      <Home />
     </div>
 
   )
