5fcff32fa19c01e92fa8b16743e156d834092c61
Vaibhav <vaibhavtiwary7766@gmail.com>
2024-07-11 14:11:36 +0530

    create message model, conversation model and send message endpoint

---
+++ backend/controllers/messageController.js
@@ -0,0 +1,31 @@
+import Conversation from "../models/conversationModel.js";
+import Message from "../models/messageModel.js";
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { message } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        let conversation = await Conversation.findOne({
+            participants: { $all: [senderId, receiverId] },
+        });
+
+        if (!conversation) {
+            conversation = await Conversation.create({
+                participants: [senderId, receiverId],
+            });
+        }
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            message,
+        });
+
+
+
+    } catch (error) {
+        console.log("Error in sendMessage controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 import authRoutes from "./routes/authroutes.js"
+import messageRoutes from "./routes/messageRoutes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -12,8 +14,10 @@ const PORT = process.env.PORT || 8000
 
 //Middlewares
 app.use(express.json());
-app.use("/api/auth", authRoutes)
+app.use(cookieParser());
 
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
 
 // app.get("/", (req, res) => {
 //     res.send("Hello World!");
